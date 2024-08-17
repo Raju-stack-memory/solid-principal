341a3afdb17fb32f44318e6f17183182da0e8095
Raju-stack-memory <rajusj15@gmail.com>
2024-08-17 15:12:49 +0530

    solid principal example

---
+++ src/main/java/com/javatechie/solid/isp/Phonepe.java
@@ -0,0 +1,12 @@
+package com.javatechie.solid.isp;
+
+public class Phonepe implements UPIPayments {
+    public void payMoney() {
+
+    }
+
+    public void getScratchCard() {
+
+    }
+
+}
---
+++ src/main/java/com/javatechie/solid/srp/BankService.java
@@ -0,0 +1,46 @@
+package com.javatechie.solid.srp;
+
+import java.util.Random;
+
+public class BankService {
+
+    public long deposit(long amount, String accountNo) {
+        //deposit amount
+        return 0;
+    }
+
+    public long withDraw(long amount, String accountNo) {
+        //withdraw amount
+        return 0;
+    }
+
+  /*  public void printPassbook() {
+        //update transaction info in passbook
+    }*/
+
+/*
+    public void sendOTP(String medium) {
+        if (medium.equals("email")) {
+            //write email related logic
+            //use JavaMailSenderAPI
+        }
+        if (medium.equals("mobile")) {
+            //write logic using twillio API
+        }
+    }
+*/
+
+
+/*    public void getLoanInterestInfo(String loanType) {
+        if (loanType.equals("homeLoan")) {
+            //do some job
+        }
+        if (loanType.equals("personalLoan")) {
+            //do some job
+        }
+        if (loanType.equals("car")) {
+            //do some job
+        }
+    }*/
+
+}
